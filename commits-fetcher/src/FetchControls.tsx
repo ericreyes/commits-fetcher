9143a014708c8bdf2a16125159c5135a2bdb139e
Eric Reyes <eric.reyes@datarobot.com>
2022-06-07 23:20:47 -0400

    Look mom, My first stab at TypeScript lol

--- commits-fetcher/src/FetchControls.tsx
+++ commits-fetcher/src/FetchControls.tsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { fetchCommitHistory } from "./actions-service";
+
+const USER_NAME = "username";
+const REPOSITORY_NAME = "repositoryName";
 
 export function FetchControls() {
-  const handleSubmission = () => {
-    console.log("asddss");
+  const [usernameQuery, setUsernameQuery] = useState("");
+  const [repositoryNameQuery, setRepositoryNameQuery] = useState("");
+
+  interface InputConfig {
+    [USER_NAME]: any;
+    [REPOSITORY_NAME]: any;
+  }
+
+  const INPUT_CONFIG: InputConfig = {
+    [USER_NAME]: {
+      setter: setUsernameQuery,
+    },
+    [REPOSITORY_NAME]: {
+      setter: setRepositoryNameQuery,
+    },
+  };
+
+  const handleSubmission = async () => {
+    const response: any = await fetchCommitHistory(
+      usernameQuery,
+      repositoryNameQuery
+    );
+    console.log(response);
   };
 
   const onInputChange = (event: any): void => {
+    // Interesting, we need `property as keyof Interface`
+    INPUT_CONFIG[event.target.id as keyof InputConfig].setter(
+      event.target.value as keyof InputConfig
+    );
+  };
+
+  const onEnter = (event: any): void => {
     if (event.keyCode === 13) {
+      console.log(event);
       handleSubmission();
     }
   };
@@ -17,20 +50,24 @@ export function FetchControls() {
         <input
           className="field-input"
           type="text"
-          id="username"
-          name="username"
+          id={USER_NAME}
+          name={USER_NAME}
           placeholder="GitHub user name"
-          onKeyDown={onInputChange}
-          test-id="username-input"
+          onKeyDown={onEnter}
+          onChange={onInputChange}
+          value={usernameQuery}
+          test-id={`${REPOSITORY_NAME}-input`}
         />
         <input
           className="field-input"
           type="text"
-          id="repo"
-          name="repo"
+          id={REPOSITORY_NAME}
+          name={REPOSITORY_NAME}
           placeholder="Repository name"
-          onKeyDown={onInputChange}
-          test-id="repo-input"
+          onKeyDown={onEnter}
+          onChange={onInputChange}
+          value={repositoryNameQuery}
+          test-id={`${REPOSITORY_NAME}-input`}
         />
         <input
           className="submit-button"
---
+++ commits-fetcher/src/actions-service.tsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export function fetchCommitHistory(username: string, repository: string) : any {
+  return Promise.resolve({ someProperty: "lol" });
+}
